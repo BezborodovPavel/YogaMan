77e74292f34f476934fc8f3c6dc04226fc0667c5
BezborodovPavel <bezborodovp@gmail.com>
2022-07-26 14:19:46 +0500

    Initial Commit

---
+++ YogaMan/YogaManApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogaManApp.swift
+//  YogaMan
+//
+//  Created by Павел on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct YogaManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
